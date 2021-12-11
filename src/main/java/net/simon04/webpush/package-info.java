428b7638605ff8c65a7d258c59deb452fc8d95a7
Simon Legner <Simon.Legner@gmail.com>
2021-12-11 20:46:35 +0100

    Add package-info.java

---
+++ src/main/java/net/simon04/webpush/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <a href="https://golb.hplar.ch/2019/08/webpush-java.html">Sending Web Push Notifications with Java</a>
+ *
+ * @apiNote https://developers.google.com/web/fundamentals/push-notifications/web-push-protocol
+ */
+package net.simon04.webpush;
